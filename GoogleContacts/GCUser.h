ccac87ad39127bb94ffabd1a123adf77aba998a5
Haja Masood <hajamasood@hotmail.com>
2014-07-17 12:41:52 -0400

    Remote users api used to populate contacts

---
+++ GoogleContacts/GCUser.h
@@ -0,0 +1,13 @@
+//
+//  GCUser.h
+//  GoogleContacts
+//
+//  Created by Haja Masood on 7/17/14.
+//  Copyright (c) 2014 Hajamasood. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GCUser : NSObject
+
+@end
---
+++ GoogleContacts/GCUser.m
@@ -0,0 +1,13 @@
+//
+//  GCUser.m
+//  GoogleContacts
+//
+//  Created by Haja Masood on 7/17/14.
+//  Copyright (c) 2014 Hajamasood. All rights reserved.
+//
+
+#import "GCUser.h"
+
+@implementation GCUser
+
+@end
